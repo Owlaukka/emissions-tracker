8d7ac5c2e33837b1fcc29df8626292abea4e5227
Owlaukka <olli.laukkanen@protonmail.com>
2019-02-28 05:35:29 +0200

    server.js modified to account for static file serving for the client side later on

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4000;
 const cors = require('cors');
@@ -7,6 +8,12 @@ const downloadJob = require('./jobs/downloadDataJob');
 const perCapitaJob = require('./jobs/calculatePerCapita');
 const routes = require('./routes');
 
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.use('/api', routes);
+
 // A job for downloading emissions and population data every day at 1:00,
 // with an error schedule of every hour at xx:05 in case of a connecton error
 downloadJob.downloadData('0 1 * * *', '5 * * * *');
@@ -14,10 +21,10 @@ downloadJob.downloadData('0 1 * * *', '5 * * * *');
 // with an error schedule of every hour at xx:07.
 perCapitaJob.calculate('2 1 * * *', '7 * * * * *');
 
-app.use(cors());
 
-app.use('/api', routes);
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.listen(PORT, function () {
     console.log('Server running on port: ', PORT)
